50d4e558b41d9fe49b9bd1963b9ca22e62b1c519
indgy <65541311+indgy@users.noreply.github.com>
2024-07-17 10:58:31 +0100

    Added more Truncate tests

--- tests/Feature/TruncateTest.php
+++ tests/Feature/TruncateTest.php
@@ -2,6 +2,26 @@
 
 test('can create truncate query', function ($dialect, $expected) {
 
+    $query = query('users')->truncate();
+    expect($query->toSql($dialect))->toBe($expected);
+
+})->with([
+            ['mysql', 'TRUNCATE `users`'],
+            ['pgsql', 'TRUNCATE "users"'],
+            ['sqlite', 'TRUNCATE "users"'],
+        ]);
+test('can create truncate query using from()', function ($dialect, $expected) {
+
+    $query = query()->from('users')->truncate();
+    expect($query->toSql($dialect))->toBe($expected);
+
+})->with([
+            ['mysql', 'TRUNCATE `users`'],
+            ['pgsql', 'TRUNCATE "users"'],
+            ['sqlite', 'TRUNCATE "users"'],
+        ]);
+test('can create truncate query using table()', function ($dialect, $expected) {
+
     $query = query()->table('users')->truncate();
     expect($query->toSql($dialect))->toBe($expected);
 
@@ -10,23 +30,6 @@ test('can create truncate query', function ($dialect, $expected) {
             ['pgsql', 'TRUNCATE "users"'],
             ['sqlite', 'TRUNCATE "users"'],
         ]);
-// test('can create delete query with where clause', function ($dialect, $data, $expected) {
 
-//     $query = query($dialect)->table('users')->delete();
-//     expect($query->toSql())->toBe($expected);
-//     expect($query->getParams())->toBe(array_values($data));
-
-// })->with([
-//             [
-//                 'sqlite',
-//                 [
-//                     'name' => 'Bob',
-//                     'email' => 'bob@example.com'
-//                 ],
-//                 "INSERT INTO users (name, email) VALUES (?, ?)"
-//             ],
-//             // ['SELECT * FROM `users`', 'mysql'],
-//             // ['SELECT * FROM "users"', 'pgsql']
-//         ]);
 
 
